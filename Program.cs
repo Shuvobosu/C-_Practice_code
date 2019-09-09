6d98bf9eba77d925a1e81e5ecf8d00144c458d98
unknown <bosu15-5645@diu.edu.bd>
2019-09-10 02:59:52 +0600

    c sharp practice code

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Name;
+            int Age;
+            string Address;
+            int Contact;
+
+            Console.WriteLine("Enter the Name: ");
+            Name = Console.ReadLine();
+
+            Console.WriteLine("Enter the Age: ");
+            Age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the Address: ");
+            Address = Console.ReadLine();
+
+            Console.WriteLine("Enter the Contact: ");
+            Contact = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Name:   " + Name);
+            Console.WriteLine("Age:   " + Age);
+            Console.WriteLine("Address:   " + Address);
+            Console.WriteLine("Contact:   " + Contact);
+
+
+            //Console.WriteLine("Name           Age      Address      Contact  ");
+            //Console.WriteLine("------------------------------------------");
+            //Console.WriteLine(Name + "       " + Age + "      " + Address + "     " + Contact);
+
+         
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
